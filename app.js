e57e79cfe30c5035e3d96c7756d9cc832e4d97ef
ellisken <ellisken@oregonstate.edu>
2018-09-17 11:22:56 -0700

    add radio button toggle

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+'use strict';
+
 //Add toggle to checkboxes
 $('#checkbox1, #checkbox2').on('click', function(){
   if($(this).hasClass('icons-check')){
     $(this).removeClass('icons-check').addClass('icons-checked');
   }else{
     $(this).removeClass('icons-checked').addClass('icons-check');
+  }
+});
+
+//Add toggle to radio buttons
+$('#radio1, #radio2').on('click', function(){
+  if($(this).hasClass('icons-radio')){
+    $(this).removeClass('icons-radio').addClass('icons-radio-selected');
+  }else{
+    $(this).removeClass('icons-radio-selected').addClass('icons-radio');
   }
 });
